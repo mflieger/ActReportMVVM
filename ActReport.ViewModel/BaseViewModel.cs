5262fa4452aa1b10a624cd1b6ea4171b599d7eaa
Marcel Flieger <fliegermar@gmail.com>
2020-10-19 02:41:37 +0200

    Add project files.

---
+++ ActReport.ViewModel/BaseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ActReport.ViewModel
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ ActReport.ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,108 @@
+﻿using ActReport.Core.Contracts;
+using ActReport.Core.Entities;
+using ActReport.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace ActReport.ViewModel
+{
+    public class EmployeeViewModel : BaseViewModel
+    {
+        private string _firstName;
+        private string _lastName;
+        private Employee _selectedEmployee;
+        private ObservableCollection<Employee> _employees;
+
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged(nameof(FirstName));
+            }
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged(nameof(LastName));
+            }
+        }
+
+        public Employee SelectedEmployee
+        {
+            get => _selectedEmployee;
+            set
+            {
+                _selectedEmployee = value;
+                FirstName = _selectedEmployee?.FirstName;
+                LastName = _selectedEmployee?.LastName;
+                OnPropertyChanged(nameof(SelectedEmployee));
+            }
+        }
+
+        public ObservableCollection<Employee> Employees
+        {
+            get => _employees;
+            set
+            {
+                _employees = value;
+                OnPropertyChanged(nameof(Employees));
+            }
+        }
+
+        public EmployeeViewModel()
+        {
+            LoadEmployees();
+        }
+
+        private void LoadEmployees()
+        {
+            using(UnitOfWork uow = new UnitOfWork())
+            {
+                var employees = uow.EmployeeRepository
+                    .Get(
+                        orderBy:
+                        coll => coll.OrderBy(emp => emp.LastName))
+                    .ToList();
+
+                Employees = new ObservableCollection<Employee>(employees);
+            }
+        }
+
+        //Commands
+        private ICommand _cmdSaveChanges;
+        public ICommand CmdSaveChanges
+        {
+            get
+            {
+                if(_cmdSaveChanges == null)
+                {
+                    _cmdSaveChanges = new RelayCommand(
+                        execute: _ =>
+                        {
+                            using IUnitOfWork uow = new UnitOfWork();
+                            _selectedEmployee.FirstName = _firstName;
+                            _selectedEmployee.LastName = _lastName;
+                            uow.EmployeeRepository.Update(_selectedEmployee);
+                            uow.Save();
+
+                            LoadEmployees();
+                        },
+                        canExecute: _ => _selectedEmployee != null);
+                }
+
+                return _cmdSaveChanges;
+            }
+            set { _cmdSaveChanges = value; }
+        }
+    }
+}
